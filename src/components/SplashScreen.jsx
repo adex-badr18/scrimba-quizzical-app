7fa90313782ea24be446cc819d88c3c17f736a99
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-07 21:07:25 +0100

    Add blobs images to splashscreen

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,11 +1,13 @@
 export default function SplashScreen() {
     return (
         <section className="splash-screen">
+            <img src="/blobs1.png" className="blobs top" alt="" />
             <h1 className="title">Quizzical</h1>
             <p className="description">
                 Your Ultimate Knowledge Challenge! 🧠 Test your wits, conquer quizzes, and become the champion of trivia!
             </p>
             <button className="start">Start quiz</button>
+            <img src="/blobs2.png" className="blobs bottom" alt="" />
         </section>
     )
 }
