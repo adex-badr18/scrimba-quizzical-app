2bbff6ad2059b5ce2bd9e6742cda2bd617664376
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-07 20:51:02 +0100

    Create Splash screen component

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,11 @@
+export default function SplashScreen() {
+    return (
+        <section className="splash-screen">
+            <h1 className="title">Quizzical</h1>
+            <p className="description">
+                Your Ultimate Knowledge Challenge! 🧠 Test your wits, conquer quizzes, and become the champion of trivia!
+            </p>
+            <button className="start">Start quiz</button>
+        </section>
+    )
+}
