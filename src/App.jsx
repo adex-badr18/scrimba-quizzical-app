d02a7b7bbb3f4e9d1c90784d37c8639acd040633
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-07 21:46:11 +0100

    Render Question component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 import SplashScreen from './components/SplashScreen';
+import Question from './components/Question';
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <div className='container'>
-            <SplashScreen />
+            {/* <SplashScreen /> */}
+            <section className="questions">
+                <Question />
+            </section>
         </div>
     )
 }
