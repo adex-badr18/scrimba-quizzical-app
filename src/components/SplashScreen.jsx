d8af6ae529da2e88f71375554f0fc58e8f2766d3
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-07 21:18:47 +0100

    Style splash header & description

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,12 +1,12 @@
 export default function SplashScreen() {
     return (
-        <section className="splash-screen">
+        <section className="splash">
             <img src="/blobs1.png" className="blobs top" alt="" />
-            <h1 className="title">Quizzical</h1>
-            <p className="description">
+            <h1 className="splash--title">Quizzical</h1>
+            <p className="splash--description">
                 Your Ultimate Knowledge Challenge! 🧠 Test your wits, conquer quizzes, and become the champion of trivia!
             </p>
-            <button className="start">Start quiz</button>
+            <button className="start-btn">Start quiz</button>
             <img src="/blobs2.png" className="blobs bottom" alt="" />
         </section>
     )
