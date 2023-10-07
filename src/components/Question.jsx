174af1e3b504e5b9a27f24b5cb604e5af13a883c
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-07 21:45:12 +0100

    Create Question component

---
+++ src/components/Question.jsx
@@ -0,0 +1,16 @@
+export default function Question() {
+    return (
+        <div className="question-container">
+            <h3 className="question">
+                Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?
+            </h3>
+
+            <ul className="options">
+                <li className="option">Cabbage Patch Kids</li>
+                <li className="option">Transformers</li>
+                <li className="option">Care Bears</li>
+                <li className="option">Rubik’s Cube</li>
+            </ul>
+        </div>
+    )
+}
