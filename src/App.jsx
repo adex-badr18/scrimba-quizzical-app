30deb674a383c033508374b342a7a520c13c09ce
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-09 02:42:30 +0100

    Set questionsArr to fetched API questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import SplashScreen from './components/SplashScreen';
 import Question from './components/Question';
 
 function App() {
     const [isSplashScreen, setIsSplashScreen] = useState(true);
+    const [questionsArr, setQuestionsArr] = useState([]);
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+            .then(res => res.json())
+            .then(data => {
+                data.response_code === 0 && setQuestionsArr(data.results);
+            })
+    }, []);
 
     function startQuiz() {
         setIsSplashScreen(false);
