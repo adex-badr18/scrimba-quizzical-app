12bc8b5e6933e4ec17fd67f5953c614759942879
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-09 04:16:03 +0100

    Decode special chars in questions & options

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,13 @@
+import { decode } from "html-entities";
+
 export default function Question(props) {
     const { question, options, answer } = props.prop;
-    const optionsJsx = options.map(option => <li className="option" key={option}>{option}</li>);
+    const optionsJsx = options.map(option => <li className="option" key={option}>{decode(option)}</li>);
 
     return (
         <div className="question-container">
             <h3 className="question">
-                {question}
+                {decode(question)}
             </h3>
 
             <ul className="options">
