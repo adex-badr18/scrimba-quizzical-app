78366ce8f0cebf98667041da7f6600a91451ce07
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-11 10:56:58 +0100

    Create quiz settings form

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,4 +1,8 @@
-export default function SplashScreen({ startQuiz }) {
+export default function SplashScreen({ startQuiz, categories }) {
+    const optionElements = categories.map(category => (
+        <option key={category.id} value={category.id}>{category.name}</option>
+    ));
+
     return (
         <section className="splash">
             <img src="/blobs1.png" className="blobs top" alt="" />
@@ -6,7 +10,23 @@ export default function SplashScreen({ startQuiz }) {
             <p className="splash--description">
                 Your Ultimate Knowledge Challenge! 🧠 Test your wits, conquer quizzes, and become the champion of trivia!
             </p>
-            <button className="start-btn" onClick={startQuiz}>Start quiz</button>
+
+            <form action="" className="settings-form">
+                <select name="category" className="category">
+                    <option value="">Any Category</option>
+                    {optionElements}
+                </select>
+
+                <select name="difficulty">
+                    <option value="">Any Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+
+                <button className="start-btn" onClick={startQuiz}>Start quiz</button>
+            </form>
+
             <img src="/blobs2.png" className="blobs bottom" alt="" />
         </section>
     )
